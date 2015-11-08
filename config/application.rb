509e590a6ef2809670c14b0ce2ba6d50d23b52ae
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-11-08 17:28:07 +0100

    creating quotes controller & using $LOAD_PATH

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 # best_quotes/config/application.rb
 require "rulers"
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+require "quotes_controller"
 
 module BestQuotes
   class Application < Rulers::Application
